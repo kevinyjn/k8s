caa1c9a721f0ffe345f4b262b14e832a7f33ca02
kevinyjn <kevinyjn@foxmail.com>
2019-08-09 00:25:55 +0800

    update

--- k8s-images.sh
+++ k8s-images.sh
@@ -5,12 +5,25 @@
 # Created on: Thu Aug  8 18:29:30 2019
 ################################################################################
 #!/bin/bash
-
-images=("coredns:1.3.1" "etcd-amd64:3.3.10" "pause:3.1" "kube-apiserver-amd64:v1.15.2" "kube-controller-manager-amd64:v1.15.2" "kube-proxy-amd64:v1.15.2" "kube-scheduler-amd64:v1.15.2")
+K8S_VERSION=v1.15.2
+ETCD_VERSION=3.3.10
+COREDNS_VERSION=1.3.1
+DASHBOARD_VERSION=v1.8.3
+FLANNEL_VERSION=v0.10.0-amd64
+DNS_VERSION=1.14.8
+PAUSE_VERSION=3.1
+images=(coredns:${COREDNS_VERSION}
+etcd-amd64:${ETCD_VERSION} etcd:${ETCD_VERSION}
+pause:${PAUSE_VERSION}
+kube-apiserver-amd64:${K8S_VERSION} kube-apiserver:${K8S_VERSION}
+kube-controller-manager-amd64:${K8S_VERSION} kube-controller-manager:${K8S_VERSION}
+kube-proxy-amd64:${K8S_VERSION} kube-proxy:${K8S_VERSION}
+kube-scheduler-amd64:${K8S_VERSION} kube-scheduler:${K8S_VERSION})
 
 for i in ${images[@]}
 do
     # echo $i
     docker pull registry.cn-beijing.aliyuncs.com/k8s_pool/$i
     docker tag registry.cn-beijing.aliyuncs.com/k8s_pool/$i k8s.gcr.io/$i
+    docker rmi registry.cn-beijing.aliyuncs.com/k8s_pool/$i
 done
